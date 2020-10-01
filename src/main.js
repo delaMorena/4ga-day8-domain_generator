28d9a6dea0056c910eb9efcaf7529d60bba78db7
delaMorena <martadmdo@gmail.com>
2020-10-01 10:52:58 +0000

    Código añadido en main.js y h1 de html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 require('./style.css')
 
 window.onload = function() { 
-    console.log('Hello world')
+    let pronombre = ["nuestra","la","aquella"]
+    let adjetivo = ["alucinante","intrigante","explosiva"]
+    let nombre = ["vida","aventura","perra"]
+    for (i = 0; i< pronombre.length; i++) {    
+        for (j = 0; j < adjetivo.length; j++) {    
+            for (k = 0; k < nombre.length; k++) {      
+                let nuestraPagina = pronombre[i] + adjetivo[j] + nombre[k] + ".com";
+                console.log(nuestraPagina);
+            }
+        }
+    }
 }
