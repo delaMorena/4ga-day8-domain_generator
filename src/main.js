a02a78b353b6d80b5dd85923fd682ebf501af3c2
delaMorena <martadmdo@gmail.com>
2020-12-01 12:00:43 +0000

    corregido

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ require('./style.css')
 window.onload = function() { 
     let pronombre = ["nuestra","la","aquella"]
     let adjetivo = ["alucinante","intrigante","explosiva"]
-    let nombre = ["vida","aventura","perra"]
+    let sustantivo = ["vida","aventura","perra"]
+    let dominio = [".com",".cu", ".co", ".fr"]
+
     for (i = 0; i< pronombre.length; i++) {    
         for (j = 0; j < adjetivo.length; j++) {    
-            for (k = 0; k < nombre.length; k++) {      
-                let nuestraPagina = pronombre[i] + adjetivo[j] + nombre[k] + ".com";
-                document.getElementById("domain").innerHTML = nuestraPagina;
+            for (k = 0; k < sustantivo.length; k++) {      
+                for (l = 0; l < dominio.length; l++) {
 
+                    console.log(pronombre[i] + adjetivo[j] + sustantivo[k] + dominio[l]);
+
+                }
+                
             }
         }
     }
